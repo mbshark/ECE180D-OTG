4df699746248385c8608b989bf5ddc5e76e85e59
Daniel Roizen <droizen@gmail.com>
2019-11-14 14:41:24 -0800

    Sever update

--- SystemExample.py
+++ SystemExample.py
@@ -2,30 +2,30 @@
 # This describes a NAIVE system design: please modify to improve robustness and client adds
 
 def declareServer(profiles):
-  # enter your codes here
-  return None
+    # enter your codes here
+    return None
 
 def perfect_grid(mapping):
-  # enter some code here to decide if rectangle
-  return False
+    # enter some code here to decide if rectangle
+    return False
 
 def optimize(mapping):
-  if perfect_grid(mapping):
-    return some_order # with PRIORITY?
+    if perfect_grid(mapping):
+        return some_order # with PRIORITY?
   
-  else:
-    return another_order
+    else:
+        return another_order
 
 def execute_to_client(order):
   # PREFERABLY MULTI-THREADED
   # ON/OFF preferably on what is in order
   # client would read output and turn LED ON/OFF
-  [send_response_to_client(ord) for ord in order]
-  return True
+    [send_response_to_client(ord) for ord in order]
+    return True
   
   
 def runServer(debug = False):
-	declareServer(profiles)
+    declareServer(profiles)
     
     # build a buffer to capture all inputs from clients
     # Assume we have i random clients CONN with n total clients
@@ -38,31 +38,31 @@ def runServer(debug = False):
     # setting up a real-time system. NOTE: You can and potentially should use
     # multi-threading to decrease latency and improve performance
     while True:
-      if wait == 0:
-      	tic += 1
-      # enter the filestream data as you get it. NOTE: You may not get it
-      try:
-      	buffer.append(filestream.entry())
+        if wait == 0:
+            tic += 1
+        # enter the filestream data as you get it. NOTE: You may not get it
+        try:
+            fer.append(filestream.entry())
       
-      # assume that the ENTRY: <IP, x, y, state>
-      # if the buffer has clients that want to connect to it
-      if not buffer:
-        for elem in buffer:
-          if elem[3] == 0:
-          	mapping[elem[0]] = elem[[1,2]]
-          	execute_to_client([elem[0], "CONFIRM"])
+        # assume that the ENTRY: <IP, x, y, state>
+        # if the buffer has clients that want to connect to it
+        if not buffer:
+            for elem in buffer:
+                if elem[3] == 0:
+                    ping[elem[0]] = elem[[1,2]]
+                    cute_to_client([elem[0], "CONFIRM"])
           
         # reset the buffer to capture new strings
-        buffer = []
+            buffer = []
       
-      # only if I have some clients, optimize and send response
-      if mapping and tic > tik_tok:
-        wait = 1
-      	order = optimize(mapping)
-        execute_to_client(order)
+        # only if I have some clients, optimize and send response
+        if mapping and tic > tik_tok:
+            wait = 1
+            er = optimize(mapping)
+            execute_to_client(order)
         
 if __name__ == "__main__":
-	runServer(debug = True)
+    runServer(debug = True)
     
     
     
--- serverConnect.py
+++ serverConnect.py
@@ -5,6 +5,7 @@ import numpy as np
 import socket
 import fcntl
 import struct
+import pickle
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,7 +19,7 @@ def get_ip_address(ifname):
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-iparray = np.empty((1,40,16),dtype=str)
+ipDict = dict()
 
 #Test/Demo Purposes
 k = 0
@@ -36,61 +37,28 @@ serv.bind((get_ip_address('wlan0'), portno))
 while True:
     while True:
         data, addr = serv.recvfrom(4096)
-        if not data: break
+        if not data: 
+            break
         data = data.decode()
-        print("Data provided is: ")
-        print(data)
-        print('\n')
+        print("Data provided is: ", data)
 
-        recData = ""
-
+        '''
         #Empty the array if the received data is a RESET function
         if(data == "RESET"):
-            iparray = np.empty((1,40,16),dtype=str)
+            iparray = np.empty((40,16),dtype=str)
             recData = "RESET"
 
         #Parse the data into position and IP address.
         else:
-            posBool = True
-            ipBool = True
-            pos = ""
-            ip = ""
-            i = 0
-            while posBool:
-                if(data[i] == ','):
-                    posBool = False
-                    i = i+1
-                    break
-                pos = pos + data[i]
-                i = i+1
-            
-            while ipBool:
-                if(i >= len(data)):
-                    ipBool = False
-                    break
-                ip = ip + data[i]
-                i = i+1
+        '''
 
-            #Add the IP address to the array of IP addresses that will later be referenced
-            for j in range (0, len(ip)):
-                iparray[(0,int(pos)-1)][j] = ip[j]
+        piNum,ip = pickle.loads(data) # data already decoded above
+        ipDict[piNum] = ip
 
-            recData = str(int(pos)+1)
-
-        print("iparray[" + str((int(pos)-1)) + "]: ")
-
-        ipAdd = ""
-
-        for l in range (0,len(iparray[0,int(pos)-1])):
-            ipAdd = ipAdd + iparray[0,int(pos)-1][l]
-
-        print(ipAdd)
-        print('\n')
+        # serv.sendto((recData).encode(),(ipAdd,8080))
 
         k = k + 1
-
-        serv.sendto((recData).encode(),(ipAdd,8080))
-
+        
         if (k > 1000):
             break
         
