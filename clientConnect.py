a8a76c57cea6d3783dd611cf4612669a3ee08f2d
Samiha Rahman <samiha.rahman@ucla.edu>
2019-11-21 17:08:46 -0800

    Refactor client

--- clientConnect.py
+++ clientConnect.py
@@ -8,65 +8,61 @@ import struct
 import pickle
 import asyncio
 
-
-def turnOnLED():
-	print("This turns on LED")
-
-def turnOffLED():
-	print("This turns off LED")
-
-#Read in IP
-def get_ip_address(ifname):
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	return socket.inet_ntoa(fcntl.ioctl(
+class Client(Thread):
+	def __init__(self, num):
+		self.piNum= num
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	
+	def get_ip_address(self, ifname):
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		return socket.inet_ntoa(fcntl.ioctl(
 		s.fileno(),
 		0x8915,  # SIOCGIFADDR
-		struct.pack('256s'.encode(), ifname[:15].encode())
-	)[20:24])
+		struct.pack('256s'.encode(), ifname[:15].encode()))[20:24])
 
+	def turnOnLED(self):
+		print("This turns on LED")
 
+	def turnOffLED(self):
+		print("This turns off LED")
 
-def runClient():
-	clientIP = get_ip_address('wlan0')
-	#Test print of IP address
+	def setupServer(self, serverIP):
+		sock = self.sock
+		clientIP = self.get_ip_address('wlan0')
+		#Test print of IP address
+		print("This is the CLIENT.")
+		print ("The client IP address is:", clientIP)
+		init_msg = pickle.dumps((self.piNum,clientIP))		
+		print("The server IP address is: ", serverIP)
 
-	print("This is the CLIENT.")
-	print ("The client IP address is:", clientIP)
+		sock.sendto(init_msg,(serverIP,8080))
 
-#Read in position(pos)
-#Use code from Charlotte
-	#This is a filler for now
-	piNum = 5
-	active = True
-	init_msg = pickle.dumps((piNum,clientIP))
-	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-	#enter server IP address - must be known beforehand
-
-	serverIP = "192.168.43.207"
-	print("The server IP address is: ", serverIP)
-	#client.connect((serverIP,8080))
-
-	client.sendto(init_msg,(serverIP,8080))
-	while True:
-		while(active):
-			print("Waiting")
-			client.settimeout(10.0)
-			data,addr = client.recvfrom(4096)
-			if not data: 
-				print('No data')
-				break
-			print("Received something")
-			#print('Received: ', pickle.loads(data))
-			signal = 0 #placeholder
-			if (signal == 1):
-				turnOnLED()
-				sleep(10) #asyncio
-				turnOffLED()
-				active = 1 #wait for response
-		#This will serve as a filler to see what we
-		print('Client closed')
-		client.close()
+	def runClient(self):	
+		sock = self.sock
+		while True:
+			while True:
+				print("Waiting")
+				sock.settimeout(10.0)
+				data,addr = sock.recvfrom(4096)
+				if not data: 
+					print('No data')
+					break
+				r_ip, r_piNum, resp =  pickle.loads(data)
+				print('Pi#: ', r_piNum)
+				print('Client IP: ', r_ip)
+				print('Server response: ', r_random)
+				signal = int(resp)#placeholder
+				if (signal == 1):
+					self.turnOnLED()
+					sleep(10) #asyncio
+					self.turnOffLED()
+					active = 1 #wait for response
+			print('Client closed')
+			sock.close()
 
 if __name__ == "__main__":
-	runClient()
+	serverIP = "192.168.43.207"
+	client = Client(5)
+	client.setupServer(serverIP)
+	client.runClient()
+
