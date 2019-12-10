f6a2757703aae9dcefa12e9af9661dd201b7bda9
Myles Byrne <myles.first@gmail.com>
2019-12-09 21:18:39 -0800

    Update serverConnect.py

--- serverConnect.py
+++ serverConnect.py
@@ -10,28 +10,30 @@ import threading
 import random
 import time
 from seatArrangement import getSeatArr
-#from seatArrangement import arrange
-#from serverConnect import connect
+from letter import findL
+
 
 def get_ip_address(ifname):
-	try:
-		import fcntl
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		return socket.inet_ntoa(fcntl.ioctl(
-			s.fileno(),
-			0x8915,  # SIOCGIFADDR
-			struct.pack('256s', ifname[:15].encode())
-		)[20:24])
-	except:
-		try: 
-			host_name = socket.gethostname() 
-			host_ip = socket.gethostbyname(host_name) 
-			print(host_ip)
-			return host_ip 
-		except: 
-			print("Unable to get Hostname and IP") 
+    try:
+        import fcntl
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15].encode())
+        )[20:24])
+    except:
+        try:
+            host_name = socket.gethostname()
+            host_ip = socket.gethostbyname(host_name)
+            print(host_ip)
+            return host_ip
+        except:
+            print("Unable to get Hostname and IP")
 
-#Test/Demo Purposes
+    # Test/Demo Purposes
+
+
 # Assigns a port for the server that listens to clients connecting to this port.
 '''
 try:
@@ -40,10 +42,9 @@ except ValueError:
 	print("This is not a valid number.")
 '''
 
-
 serv = None
 arrangement = None
-seats = None
+seats = []
 ipDict = dict()
 addrDict = dict()
 posDict = dict()
@@ -63,196 +64,205 @@ ON = 1
 OFF = 0
 ACK = 0
 
+
 def thr(i):
-	# we need to create a new loop for the thread, and set it as the 'default'
-	# loop that will be returned by calls to asyncio.get_event_loop() from this
-	# thread.
-	loop = asyncio.new_event_loop()
-	asyncio.set_event_loop(loop)
-	#loop.run_until_complete(do_stuff(i))
-	if (i == 0):
-		loop.run_until_complete(connect())
-	elif (i == 1):
-		loop.run_until_complete(arrange()) 
-	elif (i == 2):   
-		loop.run_until_complete(checkACK()) 
-	loop.close()
+    # we need to create a new loop for the thread, and set it as the 'default'
+    # loop that will be returned by calls to asyncio.get_event_loop() from this
+    # thread.
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    # loop.run_until_complete(do_stuff(i))
+    if (i == 0):
+        loop.run_until_complete(connect())
+    elif (i == 1):
+        loop.run_until_complete(arrange())
+    elif (i == 2):
+        loop.run_until_complete(checkACK())
+    loop.close()
 
 
+async def connect():
+    global serv, ipDict, portno, seats, arrangement, ACK
+    serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serv.settimeout(10)
+    serverIP = "192.168.0.238"
+    print("This is the SERVER.")
+    print("The server IP address is:", serverIP)
+    serv.bind((serverIP, portno))
+    while True:
+        while True:
+            await asyncio.sleep(random.uniform(0.1, 0.5))
+            # serv.settimeout(20)
+            try:
+                data, addr = serv.recvfrom(4096)
+                if not data:
+                    print("No Data")
+                    break
+                piNum, pos, ip = pickle.loads(data)
+                if (ip == "ACK"):
+                    # print("ACK from ",piNum)
+                    ackDict[piNum] = piNum
+                else:
+                    ipDict[piNum] = ip
+                    addrDict[piNum] = addr
+                    print(addr)
+                    posDict[piNum] = pos
+                    # ip/addr
+                    print("Data provided is: ", piNum, "   ", ip)
+                    # Verification of server-side logic
+                    rand = {1: "dino", 2: "apple", 3: "computadora", 4: "brains"}
+                    try:
+                        serv.sendto(pickle.dumps((piNum, ip, rand[piNum])), (addrDict[piNum]))
+                    except:
+                        print("Error Sending")
+            except:
 
-async def connect():	
-	global serv, ipDict, portno, seats, arrangement, ACK
-	serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	serv.settimeout(10)	
-	serverIP = "192.168.0.238"
-	print("This is the SERVER.")
-	print("The server IP address is:", serverIP)
-	serv.bind((serverIP, portno))
-	while True:
-		while True:
-			await asyncio.sleep(random.uniform(0.1, 0.5))
-			#serv.settimeout(20)
-			try:
-				data, addr = serv.recvfrom(4096)
-				if not data: 
-					print("No Data")
-					break
-				piNum,pos, ip = pickle.loads(data)
-				if (ip == "ACK"):
-					#print("ACK from ",piNum)
-					ackDict[piNum] = piNum
-				else:
-					ipDict[piNum] = ip
-					addrDict[piNum] = addr
-					print(addr)
-					posDict[piNum] = pos
-					# ip/addr
-					print("Data provided is: ", piNum, "   ", ip)
-					#Verification of server-side logic
-					rand = {1:"dino", 2:"apple", 3:"computadora"}			
-					try:		
-						serv.sendto(pickle.dumps((piNum,ip,rand[piNum])), (addrDict[piNum]))
-					except:
-						print("Error Sending")
-			except:
+                print("Client Closed")
 
-				print("Client Closed")
+            if len(posDict) >= 4:
+                seats = getSeatArr()
+                # arrangement['U'] = letter.findU(seats)
+                # arrangement['C'] = letter.findC(seats)
+                arrangement['L'] = findL(seats)
 
-			if len(posDict) >= 4:
-				seats = getSeatArr()
-				# arrangement['U'] = letter.findU(seats)
-				# arrangement['C'] = letter.findC(seats)
-				arrangement['L'] = letter.findL(seats)
-				# arrangement['A'] = letter.findA(seats)
-		await asyncio.sleep(random.uniform(0.1, 0.5))
+        # arrangement['A'] = letter.findA(seats)
+        await asyncio.sleep(random.uniform(0.1, 0.5))
 
-		
+
 def LED_state_machine():
-	global state
-	if (state == ROW_1):
-		return ROW_2
-	if (state == ROW_2):
-		return ROW_1
-	if (state == 'U'):
-		return 'C'
-	if (state == 'C'):
-		return 'L'
-	if (state == 'L'):
-		return 'A'
-	if (state == 'A'):
-		return 'U'
+    global state
+    if (state == ROW_1):
+        return ROW_2
+    if (state == ROW_2):
+        return 'L'
+    if (state == 'U'):
+        return 'C'
+    if (state == 'C'):
+        return 'L'
+    if (state == 'L'):
+        return ROW_1
+    if (state == 'A'):
+        return 'U'
 
 
 async def sendClient(msg, pi):
-	global serv, addrDict,ackDict,waitTime
-	if (msg == ON):
-		print("Turn on", pi)
-	elif (msg == OFF)	:		
-		print("Turn off", pi)
-	else:
-		print("Error")
+    global serv, addrDict, ackDict, waitTime
+    if (msg == ON):
+        print("Turn on", pi)
+    elif (msg == OFF):
+        print("Turn off", pi)
+    else:
+        print("Error")
 
-	try:		
-		serv.sendto(pickle.dumps(msg),(addrDict[pi]))
-		ackDict[pi] = -1
-		waitTime[pi] = 0
-	except:
-		print("Cound not send")
-	wait = 0
+    try:
+        serv.sendto(pickle.dumps(msg), (addrDict[pi]))
+        ackDict[pi] = -1
+        waitTime[pi] = 0
+    except:
+        print("Cound not send")
+    wait = 0
 
 
 async def sendToClients(state):
-	global seed
-	if (state == ROW_1):
-		for pi in addrDict:
-			if (posDict[pi] == "ROW_1"):
-				await sendClient(ON, pi)
-			else:
-				await sendClient(OFF, pi)
-				
-	elif (state == ROW_2):
-		for pi in addrDict:
-			if (posDict[pi] == "ROW_2"):
-				await sendClient(ON, pi)
-			else:
-				await sendClient(OFF, pi)
-	elif state == 'L':
-		seed = random.randint(0,len(arrangement['L'])-1)
-		for pi in addrDict:
-			if pi in arrangement['L'][seed].unique():
-				await sendClient(ON, pi)
+    global seed
 
-			else:
-				await sendClient(OFF, pi)
+    if (state == ROW_1):
+        for pi in addrDict:
+            if pi <= 2:
+                await sendClient(ON, pi)
+            else:
+                await sendClient(OFF, pi)
 
-	else: #unknown state turn off
-		for pi in addrDict:
-			await sendClient(OFF, pi)
-		
+    elif (state == ROW_2):
+        for pi in addrDict:
+            if pi > 2:
+                await sendClient(ON, pi)
+            else:
+                await sendClient(OFF, pi)
+    elif state == 'L':
+        if arrangement is not None:
+            seed = random.randint(0, len(arrangement['L']) - 1)
+            for pi in addrDict:
+                if arrangement is not None and pi in arrangement['L'][seed].unique():
+                    await sendClient(ON, pi)
+
+                else:
+                    await sendClient(OFF, pi)
+        else:
+            for pi in addrDict:
+                if pi != 2:
+                    await sendClient(ON, pi)
+
+                else:
+                    await sendClient(OFF, pi)
+
+    else:  # unknown state turn off
+        for pi in addrDict:
+            await sendClient(OFF, pi)
+
 
 async def checkACK():
-	global ackDict, waitTime
-	while True:
-		#print("Check")
-		delete = []
-		for pi in ackDict:
-			if ackDict[pi] == pi:
-				#print("ACK!!!")
-				delete.append(pi)
-			else:
-				waitTime[pi] += 1
-				if(waitTime[pi] > len(addrDict)+2):
-					print("Client does not exist: ", pi)
-					addrDict[pi] = -1
-					posDict[pi] = -1
-					ipDict[pi] = -1
-					delete.append(pi)
-		if (len(delete) != 0):
-			for key in delete:
-				ackDict.pop(key,None)
-				waitTime.pop(key,None)
-		cleanClients()
+    global ackDict, waitTime
+    while True:
+        # print("Check")
+        delete = []
+        for pi in ackDict:
+            if ackDict[pi] == pi:
+                # print("ACK!!!")
+                delete.append(pi)
+            else:
+                waitTime[pi] += 1
+                if (waitTime[pi] > len(addrDict) + 20):
+                    print("Client does not exist: ", pi)
+                    addrDict[pi] = -1
+                    posDict[pi] = -1
+                    ipDict[pi] = -1
+                    delete.append(pi)
+        if (len(delete) != 0):
+            for key in delete:
+                ackDict.pop(key, None)
+                waitTime.pop(key, None)
+        cleanClients()
 
-		await asyncio.sleep(random.uniform(0.1, 0.5))
-
-	
+        await asyncio.sleep(random.uniform(0.1, 0.5))
 
 
 async def arrange():
-	global arrangements, state
-	try:
-		while True:
-			#if seats:				
-			#for i in range(5 * 60 / 5):
-			await asyncio.sleep(random.uniform(1,2))
-			if (not len(ipDict) == 0):
-				state = LED_state_machine()
-			print("State is ", state)
-			await sendToClients(state)
-	except KeyboardInterrupt:
-		print('interrupted!')
+    global arrangements, state
+    try:
+        while True:
+            # if seats:
+            # for i in range(5 * 60 / 5):
+            await asyncio.sleep(random.uniform(3, 5))
+            if (not len(ipDict) == 0):
+                state = LED_state_machine()
+            print("State is ", state)
+            await sendToClients(state)
+    except KeyboardInterrupt:
+        print('interrupted!')
+
 
 def cleanClients():
-	global addrDict,posDict,ipDict
-	delete = [pi for pi in addrDict if addrDict[pi] == -1] 
-	for key in delete:
-		addrDict.pop(key,None)
-		posDict.pop(key,None)
-		ipDict.pop(key,None)
+    global addrDict, posDict, ipDict
+    delete = [pi for pi in addrDict if addrDict[pi] == -1]
+    for key in delete:
+        addrDict.pop(key, None)
+        posDict.pop(key, None)
+        ipDict.pop(key, None)
 
 
 def main():
-	num_threads = 3
-	import signal
-	signal.signal(signal.SIGINT, signal.SIG_DFL)
-	threads = [ threading.Thread(target = thr, args=(i,)) for i in range(num_threads) ]
-	[ t.start() for t in threads ]
-	[ t.join() for t in threads ]
-	print("bye")
+    num_threads = 3
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    threads = [threading.Thread(target=thr, args=(i,)) for i in range(num_threads)]
+    [t.start() for t in threads]
+    [t.join() for t in threads]
+    print("bye")
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
-#https://wiki.python.org/moin/UdpCommunication
-#https://www.geeksforgeeks.org/socket-programming-python/
+# https://wiki.python.org/moin/UdpCommunication
+# https://www.geeksforgeeks.org/socket-programming-python/
