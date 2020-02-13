801522c83cf6e5498ec094e71103e7d46ee02725
Samiha Rahman <samiha.rahman@ucla.edu>
2020-02-12 17:14:39 -0800

    Update speechClient.py

--- speechClient.py
+++ speechClient.py
@@ -5,7 +5,6 @@
 import socket
 #import fcntl
 import struct
-import pickle
 import speech_recognition as sr
 
 SETUP = 0
@@ -13,10 +12,10 @@ SPEAKING = 1
 LISTENING = 2
 CLOSED = 3
 
-serverIP = ""
-port = 5004
+serverIP = "131.179.46.131"
+port = 5000
 
-
+connected = True
 r = None
 m = None
 
@@ -25,12 +24,14 @@ class Client():
 		import signal
 		signal.signal(signal.SIGINT, signal.SIG_DFL)
 		self.piNum= num
-		self.clientIP = "192.168.43.3"
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.connect((serverIP,port))
-		self.sock.send("Hello my IP is: ", self.clientIP)
+		self.clientIP = "131.179.3.78"
+		if connected:			
+			print("Setting up server")
+			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.sock.connect((serverIP,port))
+			msg = "Hello my IP is: "+ self.clientIP
+			self.sock.send(msg.encode())
 		self.state = SETUP
-
 
 	#def get_ip_address(self, ifname):
 	#	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -40,7 +41,8 @@ class Client():
 	#		struct.pack('256s'.encode(), ifname[:15].encode()))[20:24])
 	
 	def setupServer(self, serverIP):
-		sock = self.sock#self.get_ip_address('wlan0')
+		if connected:
+			sock = self.sock#self.get_ip_address('wlan0')
 		#Test print of IP address
 		print("This is the CLIENT.")
 		print ("The client IP address is:", self.clientIP)
@@ -61,7 +63,8 @@ class Client():
 			print("Setting up failed")
 
 	def runClient(self):
-		sock = self.sock
+		if connected:
+			sock = self.sock
 		ack = str(self.piNum)
 		try:
 			try:
@@ -84,18 +87,24 @@ class Client():
 						elif (self.state == SPEAKING):
 							command = self.speak()
 							msg = str(command)#pickle.dumps((self.piNum, command))
+							m = "S,"+ msg
 							if (msg == "pause"):
-		                        print("Player wants to PAUSE the game")
-		                        sock.send(msg) 		                        
-		                    elif (msg == "play"):
-		                        print("Player wants to play the game")
-		                        sock.send(msg)
-		                    elif (msg == "hint"):
-		                        print("Player wants a hint")   
-		                        sock.send("S"+ msg)
-		                    else:
-		                        print("Not a real command to send")
-							print("Sent ", command)
+								print("Player wants to PAUSE the game")
+								if connected:
+									sock.send(m.encode())
+								print("Sent ", msg)
+							elif (msg == "play"):
+								print("Player wants to play the game")
+								if connected:
+									sock.send(m.encode())
+								print("Sent ", msg)
+							elif (msg == "hint"):
+								print("Player wants a hint")
+								if connected:
+									sock.send(m.encode())
+								print("Sent ", msg)
+							else:
+								print("Not a real command to send")
 							self.state = SPEAKING
 						elif (self.state == LISTENING):
 							print("LISTENING MODE")
@@ -103,14 +112,15 @@ class Client():
 							if not data: 
 								print('No data')
 								break
-							resp =  pickle.loads(data)
+							#resp =  pickle.loads(data)
 							print("Received ", resp)	
 							self.state = SPEAKING	
 				#ack_msg = pickle.dumps((self.piNum, "ACK"))
 				#sock.sendto(ack_msg,(serverIP,8080))
 				self.state = CLOSED
 				print('Client closed')
-				sock.close()
+				if connected:
+					sock.close()
 			except KeyboardInterrupt:
 				pass
 		except KeyboardInterrupt:
@@ -121,22 +131,21 @@ class Client():
 		global r,m
 		try:
 			while True:
-				print("Say something!")
+				print("Speaking Mode")
 				with m as source: audio = r.listen(source)
-				print("Got it! Now to recognize it...")
+				print("Processing")
 				try:
-					# recognize speech using Google Speech Recognition
+					# recognize speech using Google Web Speech
 					value = r.recognize_google(audio)
-					# we need some special handling here to correctly print unicode characters to standard output
-					if str is bytes:  # this version of Python uses bytes for strings (Python 2)
-						#print(u"You said {}".format(value).encode("utf-8"))
+					if str is bytes:
+						print(u"You said {}".format(value).encode("utf-8"))
 						return value
-					else:  # this version of Python uses unicode for strings (Python 3+)
-						#print("You said {}".format(value))
+					else:
+						print("You said {}".format(value))
 						return value
 
 				except sr.UnknownValueError:
-					print("Oops! Didn't catch that")
+					print("Unrecognized Value")
 				except sr.RequestError as e:
 					print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
 		except KeyboardInterrupt:
@@ -147,3 +156,16 @@ if __name__ == "__main__":
 	client.setupServer(serverIP)
 	client.setupSpeech()
 	client.runClient()
+
+'''
+self-generate labels
+T->Player 1
+R -> Player 2
+Star -> Play 3
+Shape4 -> Play 4
+Triangle,Square 5
+(T, 1)
+image_msg = ()
+
+msg = "({},{},{}, {},{})".format(IMU2,IMU2, IMU3, speech, image_data)
+'''
