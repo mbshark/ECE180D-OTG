86fda1c0cdad5118cc3bb78b9aa7dbd539a3a3b1
eavalo14 <avaloserick97@gmail.com>
2020-02-19 17:31:22 -0800

    Add files via upload
    updated

--- Server_Client_IMU/text_access.cs
+++ Server_Client_IMU/text_access.cs
@@ -14,8 +14,13 @@ using System.Threading;
 public class text_access : MonoBehaviour
 {
     //contents for tcp connection
-    private string roll = null;
-    private string pitch = null; 
+    private string imu_1_data = null;
+    private string imu_2_data = null;
+    private string imu_3_data = null;
+    private string imu_4_data = null;
+    private string speech_data = null;
+    private string image_data = null;
+
     private Socket clientSocket; 
 
 
@@ -45,10 +50,11 @@ public class text_access : MonoBehaviour
     panel_player_4 my_panel_script_4;
 
     //Game variables 
-    private int curr_level = 0; 
+    //private int curr_level = 0; 
     private int[] combination = new int[4];
-    private bool time_flag = false; 
-    private double start_time = 0.0; 
+    private bool[] time_flag = new bool[4];
+    private bool[] panel_passed = new bool[4];
+    private double[] start_time = new double[4];
     private double passed_time = 1.5; //time required to pass the level 
 
 
@@ -71,10 +77,11 @@ public class text_access : MonoBehaviour
         my_panel_script_4 = my_panel_4.GetComponent<panel_player_4>();
 
         // start at level one 
-        curr_level = 1; 
+        //curr_level = 1; 
 
         // generate random sequence of numbers  0-9 for 
         // players to try and accomplish
+        // initialize private variables
         int min_num = 0;
         int max_num = 9; 
         string comb = "Complete this Combination\n\t\t\t\t";
@@ -89,7 +96,9 @@ public class text_access : MonoBehaviour
             {
                 comb += combination[i].ToString(); 
             }
-            
+            time_flag[i] = false;
+            panel_passed[i]=false;
+            start_time[i]=0.0;           
             
         }
 
@@ -107,19 +116,15 @@ public class text_access : MonoBehaviour
     void CreateServer()
     {
         IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-        //Debug.Log(ipHost.AddressList[0]);
         Debug.Log(ipHost.AddressList[3]);
         //Debug.Log("Hello");
         //Debug.Log(ipHost.AddressList[2]);
         IPAddress ipAddr = ipHost.AddressList[3];
-        Debug.Log(ipAddr.AddressFamily);
-        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 5005);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 50000);
         Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
         try
         {
-
-            Debug.Log("Bind");
 
             // Using Bind() method we associate a 
             // network address to the Server Socket 
@@ -156,34 +161,16 @@ public class text_access : MonoBehaviour
                     string data = Encoding.ASCII.GetString(bytes,
                                                0, numByte);
                    // Debug.Log(data);
-
-                    if (data[0] == 'd'){
-	                    string[] words = data.Split(',');
-	                    roll = words[1];
-	                    pitch = words[2];
-
-	                    if (data.IndexOf("#") > -1)
-	                    {
-	                        Debug.Log("failed");
-	                        break;
-	                    }
-	                } else if (data[0] == 'S'){
-	                	Debug.Log(data);
-                        string[] words = data.Split(',');
-	                	string cmd = words[1];
-	                	Debug.Log(cmd);
-	                	string msg = "";
-	                	if (cmd.Equals("hint")){
-	                		msg = "Here is a very useful hint";
-	                	} else if (cmd.Equals("play")) {
-	                		msg = "Resume the game";
-						} else if (cmd.Equals("pause")){
-							msg = "Game is now PAUSED";
-						} else {
-							msg = "Client sent invalid command";
-						}
-						Debug.Log(msg);
-	                }
+                    string [] packet = data.Split('*');
+                    //Debug.Log(data);
+                    
+                    imu_1_data = packet[0];                    
+                    imu_2_data = packet[1];
+                    imu_3_data = packet[2];
+                    imu_4_data = packet[3];
+                    speech_data = packet[4];
+                    image_data = packet[5];
+                    Debug.Log(speech_data);
                     
                 }
 
@@ -214,204 +201,269 @@ public class text_access : MonoBehaviour
         }
 
     }
+    
     // Update is called once per frame
     // Updates the text that is shown for the game
+    
     void Update()
-    {
-        //converts string to float and converts to combination
-        float pitch_float = (float) Convert.ToDouble(pitch);
-        int value = (int) ((pitch_float+20)/10);
+    {       
 
         //executes levels 
-        try
-        {
-            // Update function runs even when socket is disconnected
-            // hence why i am checking for the socket connection
-            // before the levels executes 
-            if(curr_level == 1 && clientSocket.Connected == true)
-            {            
-                level_1(value, combination[curr_level-1]);
+        // Update function runs even when socket is disconnected
+        // We make sure clientSocket is connected 
+        //Debug.Log(curr_level); 
+        try{
+        
+            if((clientSocket.Connected == true )){        
+                if (imu_1_data != "R1,P1")
+                {
+                    //converts string to float and converts to combination
+                    //Debug.Log(imu_1_data);
+                    string [] roll_pitch_parsed = imu_1_data.Split(',');
+                    string pitch = roll_pitch_parsed[1];
+                    //Debug.Log(pitch);
+
+                    float pitch_float = (float) Convert.ToDouble(pitch);
+                    int value = (int) ((pitch_float+20)/10);
+
+                    level_1(value, combination[0]);
+                }
+
+                // Client Sockets are already connected no need to check 
+                // connection validity
+
+                //if (curr_level == 2)
+                //Debug.Log(imu_2_data);
+                if (imu_2_data != "R2,P2")
+                    {
+                        //converts string to float and converts to combination
+                        string [] roll_pitch_parsed = imu_2_data.Split(',');
+                        string pitch = roll_pitch_parsed[1];
+                        //Debug.Log(pitch);
+
+                        float pitch_float = (float) Convert.ToDouble(pitch);
+                        int value = (int) ((pitch_float+20)/10);
+
+                        level_2(value, combination[1]);
+                    }
+
+                // add player for player 3 
+                //if (curr_level == 3)
+                if (imu_3_data != "R3,P3")
+                    {
+                        //converts string to float and converts to combination
+                        string [] roll_pitch_parsed = imu_3_data.Split(',');
+                        string pitch = roll_pitch_parsed[1];
+                        // Debug.Log(pitch);
+
+                        float pitch_float = (float) Convert.ToDouble(pitch);
+                        int value = (int) ((pitch_float+20)/10);
+
+                        level_3(value, combination[2]);
+                    }
+
+                // add logic for player 4
+
+                //if (curr_level == 4)
+                if (imu_4_data != "R4,P4")
+                {
+                    //converts string to float and converts to combination
+                    string [] roll_pitch_parsed = imu_4_data.Split(',');
+                    string pitch = roll_pitch_parsed[1];
+                    //Debug.Log(pitch);
+
+                    float pitch_float = (float) Convert.ToDouble(pitch);
+                    int value = (int) ((pitch_float+20)/10);
+
+                    level_4(value, combination[3]);
+                } 
             }
         }
-        catch(Exception e)
-        {}
-
-        // Client Sockets are already connected no need to check 
-        // connection validity
-
-        if (curr_level == 2)
-        {
-            level_2(value, combination[curr_level-1]); 
-        }
-
-        if (curr_level == 3)
-        {
-            level_3(value, combination[curr_level-1]); 
-        }
-
-        if (curr_level == 4)
-        {
-            level_4(value, combination[curr_level-1]); 
-        }   
+        catch(Exception e){}          
 
     }
 
     // Level 1 Execution
     void level_1(float value, int combination)
     {
-        my_player_script_1.updateName(value.ToString());  
-        // compares to find when the values equal
-        if (value == combination && !time_flag)
-        {
-            start_time = Time.time;
-            time_flag = true;
-
-            // fills the borders of the panel green
-            my_panel_script_1.updatePanelGreen(false);
-        }
-        else if (value != combination)
-        {
-            time_flag = false; 
-
-            // fills the borrders of the panel red 
-            my_panel_script_1.updatePanelRed(false);
-        }
-        if (time_flag)
-        {
-            double curr_time = Time.time;
-
-            // IMU was held accordingly to the combination
-            if((curr_time-start_time) > passed_time )
+        if (!panel_passed[0]){
+            my_player_script_1.updateName(value.ToString());  
+            // compares to find when the values equal
+            if (value == combination && !time_flag[0])
             {
-                // increase level
-                curr_level = 2; 
+                start_time[0] = Time.time;
+                time_flag[0] = true;
 
-            // fills the panel green
-            my_panel_script_1.updatePanelGreen(true);
-            my_player_script_1.updateName(value.ToString()); 
+                // fills the borders of the panel green
+                my_panel_script_1.updatePanelGreen(false);
+            }
+            else if (value != combination)
+            {
+                time_flag[0] = false; 
 
-            time_flag = false; 
-            
+                // fills the borrders of the panel red 
+                my_panel_script_1.updatePanelRed(false);
+            }
+            if (time_flag[0])
+            {
+                double curr_time = Time.time;
+
+                // IMU was held accordingly to the combination
+                if((curr_time-start_time[0]) > passed_time )
+                {
+                    // increase level
+                    //curr_level = 2; 
+
+                    // fills the panel green
+                    my_panel_script_1.updatePanelGreen(true);
+                    my_player_script_1.updateName(value.ToString()); 
+
+                    time_flag[0] = false; 
+                    panel_passed[0]=true;
+                
+                }
             }
         }
+        // does nothing
+        else{}
     }
     // Level 2 execution    
     void level_2(float value, int combination)
     {
-        my_player_script_2.updateName(value.ToString());  
-        // compares to find when the values equal
-        if (value == combination && !time_flag)
-        {
-            start_time = Time.time;
-            time_flag = true;
-
-            // fills the borders of the panel green
-            my_panel_script_2.updatePanelGreen(false);
-
-        }
-        else if (value != combination)
-        {
-            //Debug.Log("Not correct combination");
-            time_flag = false; 
-
-            // fills the borrders of the panel red 
-            my_panel_script_2.updatePanelRed(false);
-        }
-        if (time_flag)
-        {
-            double curr_time = Time.time;
-
-            // IMU was held accordingly to the combination
-            if((curr_time-start_time) > passed_time )
+        if (!panel_passed[1]){
+            my_player_script_2.updateName(value.ToString());  
+            // compares to find when the values equal
+            if (value == combination && !time_flag[1])
             {
-                // increase level
-                curr_level = 3; 
+                start_time[1] = Time.time;
+                time_flag[1] = true;
 
-                // fills the panel green
-                my_panel_script_2.updatePanelGreen(true);
-                my_player_script_2.updateName(value.ToString());  
+                // fills the borders of the panel green
+                my_panel_script_2.updatePanelGreen(false);
+
+            }
+            else if (value != combination)
+            {
+                //Debug.Log("Not correct combination");
+                time_flag[1] = false; 
+
+                // fills the borrders of the panel red 
+                my_panel_script_2.updatePanelRed(false);
+            }
+            if (time_flag[1])
+            {
+                double curr_time = Time.time;
+
+                // IMU was held accordingly to the combination
+                if((curr_time-start_time[1]) > passed_time )
+                {
+                    // increase level
+                    //curr_level = 3; 
+
+                    // fills the panel green
+                    my_panel_script_2.updatePanelGreen(true);
+                    my_player_script_2.updateName(value.ToString());  
 
 
-                time_flag = false; 
+                    time_flag[1] = false;
+                    panel_passed[1] = true; 
+                }
             }
         }
+        //do nothing
+        else{}
     }
+
+
 
     // Level 3 Execution
     void level_3(float value, int combination)
     {
-        my_player_script_3.updateName(value.ToString());  
-        // compares to find when the values equal
-        if (value == combination && !time_flag)
+        if(!panel_passed[2])
         {
-            start_time = Time.time;
-            time_flag = true;
-
-            // fills the borders of the panel green
-            my_panel_script_3.updatePanelGreen(false);
-        }
-        else if (value != combination)
-        {
-            time_flag = false; 
-
-            // fills the borrders of the panel red 
-            my_panel_script_3.updatePanelRed(false);
-        }
-        if (time_flag)
-        {
-            double curr_time = Time.time;
-
-            // IMU was held accordingly to the combination
-            if((curr_time-start_time) > passed_time )
+            my_player_script_3.updateName(value.ToString());  
+            // compares to find when the values equal
+            if (value == combination && !time_flag[2])
             {
-                // increase level
-                curr_level = 4; 
+                start_time[2] = Time.time;
+                time_flag[2] = true;
 
-                // fills the panel green
-                my_panel_script_3.updatePanelGreen(true);
+                // fills the borders of the panel green
+                my_panel_script_3.updatePanelGreen(false);
+            }
+            else if (value != combination)
+            {
+                time_flag[2] = false; 
 
-                time_flag = false; 
+                // fills the borrders of the panel red 
+                my_panel_script_3.updatePanelRed(false);
+            }
+            if (time_flag[2])
+            {
+                double curr_time = Time.time;
+
+                // IMU was held accordingly to the combination
+                if((curr_time-start_time[2]) > passed_time )
+                {
+                    // increase level
+                    //curr_level = 4; 
+
+                    // fills the panel green
+                    my_panel_script_3.updatePanelGreen(true);
+
+                    time_flag[2] = false;
+                    panel_passed[2] = true;
+                }
             }
         }
+        //do nothing
+        else{}
     }
 
      // Level 4 Execution
     void level_4(float value, int combination)
     {
-        my_player_script_4.updateName(value.ToString());  
-        // compares to find when the values equal
-        if (value == combination && !time_flag)
+        if (!panel_passed[3])
         {
-            start_time = Time.time;
-            time_flag = true;
-
-            // fills the borders of the panel green
-            my_panel_script_4.updatePanelGreen(false);
-        }
-        else if (value != combination)
-        {
-            time_flag = false; 
-
-            // fills the borrders of the panel red 
-            my_panel_script_4.updatePanelRed(false);
-        }
-        if (time_flag)
-        {
-            double curr_time = Time.time;
-
-            // IMU was held accordingly to the combination
-            if((curr_time-start_time) > passed_time )
+            my_player_script_4.updateName(value.ToString());  
+            // compares to find when the values equal
+            if (value == combination && !time_flag[3])
             {
-                // increase level
-                curr_level = 5; 
+                start_time[3] = Time.time;
+                time_flag[3] = true;
 
-                // fills the panel green
-                my_panel_script_4.updatePanelGreen(true);
+                // fills the borders of the panel green
+                my_panel_script_4.updatePanelGreen(false);
+            }
+            else if (value != combination)
+            {
+                time_flag[3] = false; 
 
-                time_flag = false; 
+                // fills the borrders of the panel red 
+                my_panel_script_4.updatePanelRed(false);
+            }
+            if (time_flag[3])
+            {
+                double curr_time = Time.time;
+
+                // IMU was held accordingly to the combination
+                if((curr_time-start_time[3]) > passed_time )
+                {
+                    // increase level
+                    //curr_level = 5; 
+
+                    // fills the panel green
+                    my_panel_script_4.updatePanelGreen(true);
+
+                    time_flag[3] = false;
+                    panel_passed[3] = true; 
+                }
             }
         }
+        //do nothing
+        else{}
     }
+    
 
 
 }
