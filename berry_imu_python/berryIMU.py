6483a1834ff80a469b941cd12d37bf897b7225b0
eavalo14 <avaloserick97@gmail.com>
2020-02-21 21:18:36 -0800

    Add files via upload

--- berry_imu_python/berryIMU.py
+++ berry_imu_python/berryIMU.py
@@ -90,7 +90,7 @@ def MadgwickQuaternionUpdate(ax,ay,az,gx,gy,gz,mx,my,mz):
     # Normalise magnetometer measurement
     norm = math.sqrt(mx * mx + my * my + mz * mz)
     if (norm == float(0.0)):
-         return  #// handle NaN
+         return  # handle NaN
     norm = float(1.0)/norm
     mx *= norm
     my *= norm
@@ -102,45 +102,43 @@ def MadgwickQuaternionUpdate(ax,ay,az,gx,gy,gz,mx,my,mz):
     _2q1mz = float(2.0) * q1 * mz
     _2q2mx = float(2.0) * q2 * mx
 
-    '''
-    hx = mx * q1q1 - _2q1my * q4 + _2q1mz * q3 + mx * q2q2 + _2q2 * my * q3 + _2q2 * mz * q4 - mx * q3q3 - mx * q4q4;
-    hy = _2q1mx * q4 + my * q1q1 - _2q1mz * q2 + _2q2mx * q3 - my * q2q2 + my * q3q3 + _2q3 * mz * q4 - my * q4q4;
-    _2bx = sqrt(hx * hx + hy * hy);
-    _2bz = -_2q1mx * q3 + _2q1my * q2 + mz * q1q1 + _2q2mx * q4 - mz * q2q2 + _2q3 * my * q4 - mz * q3q3 + mz * q4q4;
-    _4bx = 2.0f * _2bx;
-    _4bz = 2.0f * _2bz;
+    
+    hx = mx * q1q1 - _2q1my * q4 + _2q1mz * q3 + mx * q2q2 + _2q2 * my * q3 + _2q2 * mz * q4 - mx * q3q3 - mx * q4q4
+    hy = _2q1mx * q4 + my * q1q1 - _2q1mz * q2 + _2q2mx * q3 - my * q2q2 + my * q3q3 + _2q3 * mz * q4 - my * q4q4
+    _2bx = sqrt(hx * hx + hy * hy)
+    _2bz = -_2q1mx * q3 + _2q1my * q2 + mz * q1q1 + _2q2mx * q4 - mz * q2q2 + _2q3 * my * q4 - mz * q3q3 + mz * q4q4
+    _4bx = float(2.0) * _2bx
+    _4bz = float(2.0) * _2bz
 
-    // Gradient decent algorithm corrective step
-    s1 = -_2q3 * (2.0f * q2q4 - _2q1q3 - ax) + _2q2 * (2.0f * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-    s2 = _2q4 * (2.0f * q2q4 - _2q1q3 - ax) + _2q1 * (2.0f * q1q2 + _2q3q4 - ay) - 4.0f * q2 * (1.0f - 2.0f * q2q2 - 2.0f * q3q3 - az) + _2bz * q4 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q3 + _2bz * q1) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q4 - _4bz * q2) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-    s3 = -_2q1 * (2.0f * q2q4 - _2q1q3 - ax) + _2q4 * (2.0f * q1q2 + _2q3q4 - ay) - 4.0f * q3 * (1.0f - 2.0f * q2q2 - 2.0f * q3q3 - az) + (-_4bx * q3 - _2bz * q1) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q2 + _2bz * q4) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q1 - _4bz * q3) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-    s4 = _2q2 * (2.0f * q2q4 - _2q1q3 - ax) + _2q3 * (2.0f * q1q2 + _2q3q4 - ay) + (-_4bx * q4 + _2bz * q2) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q1 + _2bz * q3) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q2 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
-    norm = sqrt(s1 * s1 + s2 * s2 + s3 * s3 + s4 * s4);    // normalise step magnitude
-    norm = 1.0f/norm;
-    s1 *= norm;
-    s2 *= norm;
-    s3 *= norm;
-    s4 *= norm;
+    # Gradient decent algorithm corrective step
+    s1 = -_2q3 * (float(2.0) * q2q4 - _2q1q3 - ax) + _2q2 * (float(2.0) * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (float(0.5) - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (float(0.5) - q2q2 - q3q3) - mz)
+    s2 = _2q4 * (float(2.0) * q2q4 - _2q1q3 - ax) + _2q1 * (float(2.0) * q1q2 + _2q3q4 - ay) - float(4.0) * q2 * (float(1.0) - float(2.0) * q2q2 - float(2.0) * q3q3 - az) + _2bz * q4 * (_2bx * (float(0.5) - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q3 + _2bz * q1) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q4 - _4bz * q2) * (_2bx * (q1q3 + q2q4) + _2bz * (float(0.5) - q2q2 - q3q3) - mz)
+    s3 = -_2q1 * (float(2.0) * q2q4 - _2q1q3 - ax) + _2q4 * (float(2.0) * q1q2 + _2q3q4 - ay) - float(4.0) * q3 * (float(1.0) - float(2.0) * q2q2 - float(2.0) * q3q3 - az) + (-_4bx * q3 - _2bz * q1) * (_2bx * (float(0.5) - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q2 + _2bz * q4) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q1 - _4bz * q3) * (_2bx * (q1q3 + q2q4) + _2bz * (float(0.5) - q2q2 - q3q3) - mz)
+    s4 = _2q2 * (float(2.0) * q2q4 - _2q1q3 - ax) + _2q3 * (float(2.0) * q1q2 + _2q3q4 - ay) + (-_4bx * q4 + _2bz * q2) * (_2bx * (float(0.5) - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q1 + _2bz * q3) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q2 * (_2bx * (q1q3 + q2q4) + _2bz * (float(0.5) - q2q2 - q3q3) - mz)
+    norm = math.sqrt(s1 * s1 + s2 * s2 + s3 * s3 + s4 * s4) # normalise step magnitude
+    norm = float(1.0)/norm
+    s1 *= norm
+    s2 *= norm
+    s3 *= norm
+    s4 *= norm
 
-    // Compute rate of change of quaternion
-    qDot1 = 0.5f * (-q2 * gx - q3 * gy - q4 * gz) - beta * s1;
-    qDot2 = 0.5f * (q1 * gx + q3 * gz - q4 * gy) - beta * s2;
-    qDot3 = 0.5f * (q1 * gy - q2 * gz + q4 * gx) - beta * s3;
-    qDot4 = 0.5f * (q1 * gz + q2 * gy - q3 * gx) - beta * s4;
+    # Compute rate of change of quaternion
+    qDot1 = float(0.5) * (-q2 * gx - q3 * gy - q4 * gz) - beta * s1
+    qDot2 = float(0.5) * (q1 * gx + q3 * gz - q4 * gy) - beta * s2
+    qDot3 = float(0.5) * (q1 * gy - q2 * gz + q4 * gx) - beta * s3
+    qDot4 = float(0.5) * (q1 * gz + q2 * gy - q3 * gx) - beta * s4
 
-    // Integrate to yield quaternion
-    q1 += qDot1 * deltat;
-    q2 += qDot2 * deltat;
-    q3 += qDot3 * deltat;
-    q4 += qDot4 * deltat;
-    norm = sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    // normalise quaternion
-    norm = 1.0f/norm;
-    q[0] = q1 * norm;
-    q[1] = q2 * norm;
-    q[2] = q3 * norm;
-    q[3] = q4 * norm;
-
-    '''
+    # Integrate to yield quaternion
+    q1 += qDot1 * deltat
+    q2 += qDot2 * deltat
+    q3 += qDot3 * deltat
+    q4 += qDot4 * deltat
+    norm = math.sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    # normalise quaternion
+    norm = float(1.0)/norm
+    q[0] = q1 * norm
+    q[1] = q2 * norm
+    q[2] = q3 * norm
+    q[3] = q4 * norm
     
 
 
