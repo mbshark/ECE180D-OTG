e4fb5beb46e61835d24cbb0a60236b0b55bd1de1
eavalo14 <avaloserick97@gmail.com>
2020-02-21 21:01:24 -0800

    Add files via upload

--- berry_imu_python/berryIMU.py
+++ berry_imu_python/berryIMU.py
@@ -29,25 +29,40 @@ mRes = 0.000489
 
 
 def MadgwickQuaternionUpdate(ax,ay,az,gx,gy,gz,mx,my,mz):
-    q1 = q[0], q2 = q[1], q3 = q[2], q4 = q[3];  # // short name local variable for readability
-    print(type(q1))
-    '''
-    float norm;
-    float hx, hy, _2bx, _2bz;
-    float s1, s2, s3, s4;
-    float qDot1, qDot2, qDot3, qDot4;
+    q1 = q[0]
+    q2 = q[1]
+    q3 = q[2]
+    q4 = q[3]
+    norm = float(0.0)
+    hx = float(0.0)
+    hy = float(0.0)
+    _2bx = float(0.0)
+    _2bz = float(0.0)
+    s1 = float(0.0)
+    s2 = float(0.0)
+    s3 = float(0.0)
+    s4 = float(0.0)
+    qDot1 = float(0.0)
+    qDot2 = float(0.0)
+    qDot3 = float(0.0)
+    qDot4 = float(0.0)
 
-    // Auxiliary variables to avoid repeated arithmetic
-    float _2q1mx;
-    float _2q1my;
-    float _2q1mz;
-    float _2q2mx;
-    float _4bx;
-    float _4bz;
-    float _2q1 = 2.0f * q1;
-    float _2q2 = 2.0f * q2;
-    float _2q3 = 2.0f * q3;
-    float _2q4 = 2.0f * q4;
+
+    # Auxiliary variables to avoid repeated arithmetic
+    _2q1mx = float(0.0)
+    _2q1mx = float(0.0)
+    _2q1mz = float(0.0)
+    _2q2mx = float(0.0)
+    _4bx = float(0.0)
+    _4bz = float(0.0)
+    _2q1 = float(float(2.0) * q1)
+    _2q2 = float(float(2.0) * q2)
+    _2q3 = float(float(2.0) * q3)
+    _2q4 = float(float(2.0) * q4)
+    _2q1q3 = float(float(2.0) * q1 * q3)
+
+    '''
+
     float _2q1q3 = 2.0f * q1 * q3;
     float _2q3q4 = 2.0f * q3 * q4;
     float q1q1 = q1 * q1;
@@ -60,7 +75,10 @@ def MadgwickQuaternionUpdate(ax,ay,az,gx,gy,gz,mx,my,mz):
     float q3q3 = q3 * q3;
     float q3q4 = q3 * q4;
     float q4q4 = q4 * q4;
+    '''
 
+
+f   '''
     // Normalise accelerometer measurement
     norm = sqrt(ax * ax + ay * ay + az * az);
     if (norm == 0.0f) return; // handle NaN
@@ -118,7 +136,9 @@ def MadgwickQuaternionUpdate(ax,ay,az,gx,gy,gz,mx,my,mz):
     q[1] = q2 * norm;
     q[2] = q3 * norm;
     q[3] = q4 * norm;
+
     '''
+    
 
 
 
