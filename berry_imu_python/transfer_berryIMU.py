3b76360785481a42c2da8be8bac0d5abd4cd828f
eavalo14 <avaloserick97@gmail.com>
2020-02-22 00:05:47 -1000

    changes

--- berry_imu_python/transfer_berryIMU.py
+++ berry_imu_python/transfer_berryIMU.py
@@ -162,8 +162,9 @@ while True:
 
     if(ser.in_waiting >0):
 		data= ser.readline()
-        data = data.encode('utf-8')
-        data_splt = data.split(",")
+		print(data)
+       		data = data.encode('utf-8').strip()
+        	data_splt = data.split(",")
 		
 		print(data_splt)
 
